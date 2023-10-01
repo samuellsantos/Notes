4e9e0d40d75bc53e647d0f0862e642d312dcc4e4
Samuel Santos <samuka0102128@gmail.com>
2023-10-01 20:37:08 -0300

    Add poppins font

---
+++ src/components/NotesAPP.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SideMenu } from './SideMenu'
+
+export const NotesAPP = () => {
+  return (
+    <div>
+      <SideMenu />
+    </div>
+  )
+}
---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SideMenu = () => {
+  return (
+    <div className='font-poppins font-medium'>
+      Notes
+    </div>
+  )
+}
