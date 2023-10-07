8912b83f085aaf9280de31be888ac84090ee160e
Samuel Santos <samuka0102128@gmail.com>
2023-10-06 23:34:22 -0300

    Animation Sidemenu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
+import { motion, AnimatePresence } from 'framer-motion'
 import { ColorSelect } from './ColorSelect'
 
 export const SideMenu = () => {
@@ -14,20 +15,37 @@ export const SideMenu = () => {
       <div className='text-2xl font-bold'>
         Notes
       </div>
-      <div className={`bg-black p-4 rounded-full cursor-pointer hover:animate-pulse ${displayColor && 'rotate-45'} transition-transform`} onClick={toggleColor}>
+      <div className={`bg-black p-4 rounded-full cursor-pointer hover:opacity-70 ${displayColor && 'rotate-45'} transition-colors`} onClick={toggleColor}>
         <AiOutlinePlus color='white' size='20'/>
       </div>
 
-      {
-        displayColor && 
-        <div className='flex flex-col items-center gap-4'>
-          <ColorSelect color='bg-yellow-500'/>
-          <ColorSelect color='bg-rose-400'/>
-          <ColorSelect color='bg-fuchsia-400'/>
-          <ColorSelect color='bg-cyan-300'/>
-          <ColorSelect color='bg-lime-300'/>
-      </div>
-      }
+      <AnimatePresence>
+        {displayColor && (
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
+            className='flex flex-col items-center gap-4'
+          >
+            <motion.div key='yellow' custom={1} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.5, delay: 0.1 }}>
+              <ColorSelect color='bg-yellow-500' />
+            </motion.div>
+            <motion.div key='rose' custom={2} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.5, delay: 0.2 }}>
+              <ColorSelect color='bg-rose-400' />
+            </motion.div>
+            <motion.div key='fuchsia' custom={3} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.5, delay: 0.3 }}>
+              <ColorSelect color='bg-fuchsia-400' />
+            </motion.div>
+            <motion.div key='cyan' custom={4} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.5, delay: 0.4 }}>
+              <ColorSelect color='bg-cyan-300' />
+            </motion.div>
+            <motion.div key='lime' custom={5} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.5, delay: 0.5 }}>
+              <ColorSelect color='bg-lime-300' />
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
