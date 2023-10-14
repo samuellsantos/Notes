c026f5b49dd58adb5cdf2929eccf229ed90a6158
Samuel Santos <samuka0102128@gmail.com>
2023-10-14 18:16:51 -0300

    Create Big Note template

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -28,21 +28,31 @@ export const SideMenu = () => {
             transition={{ duration: 0.5 }}
             className='flex flex-col items-center gap-4'
           >
-            <motion.div key='yellow' custom={1} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.1, delay: 0.1 }}>
+            <motion.div key='yellow' custom={1} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.1, delay: 0.1 }} >
               <ColorSelect color='bg-yellow-500' />
             </motion.div>
+
+
             <motion.div key='rose' custom={2} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.1, delay: 0.2 }}>
               <ColorSelect color='bg-rose-400' />
             </motion.div>
+
+
             <motion.div key='fuchsia' custom={3} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.1, delay: 0.3 }}>
               <ColorSelect color='bg-fuchsia-400' />
             </motion.div>
+
+
             <motion.div key='cyan' custom={4} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.1, delay: 0.4 }}>
               <ColorSelect color='bg-cyan-300' />
             </motion.div>
+
+
             <motion.div key='lime' custom={5} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -20 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.1, delay: 0.5 }}>
               <ColorSelect color='bg-lime-300' />
             </motion.div>
+
+
           </motion.div>
         )}
       </AnimatePresence>
