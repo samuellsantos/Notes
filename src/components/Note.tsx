4ff8fc1a1a13a84b751e1bc90869ee8f0a03c370
Samuel Santos <samuka0102128@gmail.com>
2023-11-12 11:45:35 -0300

    Option button

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,67 +1,80 @@
-import { useState } from 'react'
-import { OptionsButton } from './OptionsButton'
-import { removeNote } from '../redux/saveNoteSlice'
-import { useDispatch } from 'react-redux'
-import { motion, AnimatePresence } from 'framer-motion'
+import { useState } from 'react';
+import { OptionsButton } from './OptionsButton';
+import { removeNote } from '../redux/saveNoteSlice';
+import { useDispatch } from 'react-redux';
+import { motion, AnimatePresence } from 'framer-motion';
 
-export const Note = ({color, children, id}: any ) => {
-  const [openOptions, setOpenOptions] = useState(false)
-  const dispatch = useDispatch()
+export const Note = ({ color, children, id }: any) => {
+  const [openOptions, setOpenOptions] = useState(false);
+  const dispatch = useDispatch();
 
   const handleOpenOptions = () => {
-    setOpenOptions(!openOptions)
-  }
+    setOpenOptions(!openOptions);
+  };
 
   const handleRemoveNote = () => {
-    dispatch(removeNote(id))
-    console.log(id)
-  }
-  
+    dispatch(removeNote(id));
+  };
+
   return (
-   <div className='relative transition-all'>
-      <div  className={`${color} rounded p-4 max-w-2xl h-auto flex items-center z-20`}>
-        <p className='max-w-xs break-words'>
-          {children}
-        </p>
-        <div onClick={handleOpenOptions} className='
-        self-end'>
-           <OptionsButton />
+    <div className="relative transition-all">
+      <div
+        className={`${color} rounded p-4 max-w-2xl h-auto flex items-center z-20 transition-`}
+      >
+        <p className={`max-w-xs break-words pr-16`}>{children}</p>
+
+        <div
+          onClick={handleOpenOptions}
+          className={`self-end ${
+            openOptions && 'opacity-0'
+          } transition-opacity`}
+        >
+          <OptionsButton />
         </div>
 
-    {/* OPTIONS */}
-  <AnimatePresence>
-    {openOptions && 
-      <div className='absolute -right-24 top-0 z-50'>
-          <div className='flex flex-col items-start gap-y-2'>
-          <motion.button 
-          onClick={handleRemoveNote}
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.5 }} className='p-1 bg-black text-white rounded-sm cursor-pointer w-20'>Delete
-          </motion.button >
+        {/* OPTIONS */}
+        <AnimatePresence>
+          {openOptions && (
+            <div className="absolute right-2 top-0 z-50">
+              <div className="flex flex-col items-start gap-y-2">
+                <motion.button
+                  onClick={handleRemoveNote}
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  transition={{ duration: 0.5 }}
+                  className="p-1 bg-black text-white rounded-sm cursor-pointer w-20"
+                >
+                  Delete
+                </motion.button>
 
-          <motion.button 
-            custom={1} 
-            animate={{ opacity: 1, y: 0 }} 
-            initial={{ opacity: 0, y: -20 }} 
-            exit={{ opacity: 0, y: -20 }} 
-            transition={{ duration: 0.1, delay: 0.1 }} 
-            className='p-1 bg-black text-white rounded-sm cursor-pointer w-20'>Edit</motion.button >
+                <motion.button
+                  custom={1}
+                  animate={{ opacity: 1, y: 0 }}
+                  initial={{ opacity: 0, y: -20 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  transition={{ duration: 0.1, delay: 0.1 }}
+                  className="p-1 bg-black text-white rounded-sm cursor-pointer w-20"
+                >
+                  Edit
+                </motion.button>
 
-            <motion.button 
-            onClick={handleOpenOptions}
-            custom={2} 
-            animate={{ opacity: 1, y: 0 }}
-            initial={{ opacity: 0, y: -20 }} 
-            exit={{ opacity: 0, y: -20 }} 
-            transition={{ duration: 0.1, delay: 0.2 }} 
-            className='p-1 bg-black text-white rounded-sm cursor-pointer w-20'>Cancel</motion.button >
-          </div>
-        </div>
-      }
-    </AnimatePresence>
+                <motion.button
+                  onClick={handleOpenOptions}
+                  custom={2}
+                  animate={{ opacity: 1, y: 0 }}
+                  initial={{ opacity: 0, y: -20 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  transition={{ duration: 0.1, delay: 0.2 }}
+                  className="p-1 bg-black text-white rounded-sm cursor-pointer w-20"
+                >
+                  Cancel
+                </motion.button>
+              </div>
+            </div>
+          )}
+        </AnimatePresence>
       </div>
-   </div>
-  )
-}
+    </div>
+  );
+};
--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,50 +1,58 @@
-import { useState } from 'react'
-import { BigNote } from './BigNote'
-import { Note } from './Note'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '../redux/store'
-import { removeNote } from '../redux/saveNoteSlice'
+import { useState } from 'react';
+import { BigNote } from './BigNote';
+import { Note } from './Note';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import { removeNote } from '../redux/saveNoteSlice';
 
-import notesImage from '../assets/Add notes.gif'
+import notesImage from '../assets/Add notes.gif';
 
 export const Notes = () => {
-  const notesFromRedux = useSelector((state: RootState) => state.save)
-  const [delNote, setDelNote] = useState(false)
+  const notesFromRedux = useSelector((state: RootState) => state.save);
+  const [delNote, setDelNote] = useState(false);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   function noteID(id: number) {
-    if(delNote) {
-      const saveID = id
-      dispatch(removeNote(saveID))
-      setDelNote(false)
+    if (delNote) {
+      const saveID = id;
+      dispatch(removeNote(saveID));
+      setDelNote(false);
     }
   }
 
   return (
-    <div className='w-full h-full relative'>
-      <header className='flex items-center justify-between'>
+    <div className="w-full h-full relative">
+      <header className="flex items-center justify-between">
         <div>
-          <h1 className='text-3xl mb-4'>
-            Notes
-          </h1>
+          <h1 className="text-3xl mb-4">Notes</h1>
         </div>
-
       </header>
 
-      <div className='flex flex-wrap gap-3'>
-      {notesFromRedux.length > 0 ? notesFromRedux.map((note: any) => (
-          <Note color={note.color} key={note.id} id={note.id} onClick={() => noteID(note.id)}>{note.note}</Note>
-      ))
-      : <div className='
-      w-full h-full flex items-center justify-center'>
-          <img src={notesImage} alt="Add your note now" />
-        </div>}
-       
+      <div className="flex flex-wrap gap-3">
+        {notesFromRedux.length > 0 ? (
+          notesFromRedux.map((note: any) => (
+            <Note
+              color={note.color}
+              key={note.id}
+              id={note.id}
+              onClick={() => noteID(note.id)}
+            >
+              {note.note}
+            </Note>
+          ))
+        ) : (
+          <div
+            className="
+      w-full h-full flex items-center justify-center"
+          >
+            <img src={notesImage} alt="Add your note now" />
+          </div>
+        )}
       </div>
-      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <BigNote />
       </div>
     </div>
-  )
-}
+  );
+};
